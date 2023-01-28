6a42f926289a6509ab31e02ac118d79494d490fc
mas-ky <keymas702@gmail.com>
2023-01-28 08:14:51 +0700

    dasaran

---
+++ src/main/java/com/kirimpesantelegram/kirimpesantelegram/KirimpesantelegramApplication.java
@@ -0,0 +1,13 @@
+package com.kirimpesantelegram.kirimpesantelegram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KirimpesantelegramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KirimpesantelegramApplication.class, args);
+	}
+
+}
